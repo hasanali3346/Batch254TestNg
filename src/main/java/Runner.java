07d1d311ab0e6db2d21938513f3f4e7772cb76dd
Hasan Ali DONMEZ <hasanali12091@gmail.com>
2024-09-19 15:12:00 +0300

    v3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,7 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("versiyon 1");
-        System.out.println("versiyon 1");
+        System.out.println("versiyon 2");
+        System.out.println("versiyon 3");
 
     }
 
