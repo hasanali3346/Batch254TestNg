f4438dda82479931d6c82594a1640a94e9977e18
Hasan Ali DONMEZ <hasanali12091@gmail.com>
2024-09-19 15:18:27 +0300

    tech1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,6 +3,7 @@ public class Runner {
         System.out.println("versiyon 1");
         System.out.println("versiyon 2");
         System.out.println("versiyon 3");
+        System.out.println("techpro 1");
 
     }
 
